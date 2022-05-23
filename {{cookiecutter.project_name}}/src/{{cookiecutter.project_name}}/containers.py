03297c3a8d9223a688d014c8c2e84d42a90376a3
Sergio León <panicoenlaxbox@gmail.com>
2022-05-23 09:26:26 +0200

    Update containers.py

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/containers.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/containers.py
@@ -1,21 +1,16 @@
 import logging.config
-from pathlib import Path
 
 import yaml
 from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
 
 
-def _get_path(file: str) -> Path:
-    return Path(__file__).resolve().parent / file
-
-
-def _init_logging():
-    with open(_get_path("logging.yml"), "r") as f:
+def _logging_resource():
+    with open("logging.yml", "r") as f:
         logging.config.dictConfig(yaml.safe_load(f.read()))
 
 
 class Container(DeclarativeContainer):
-    configure_logging = providers.Resource(_init_logging)
+    _logging_resource = providers.Resource(_logging_resource)
     config = providers.Configuration()
-    config.from_yaml(_get_path("config.yml"), required=True)  # noqa: CCE002
+    config.from_yaml("config.yml", required=True)  # noqa: CCE002
