df0545e72206398b1b3b370d0a6e1fb91ade11c0
Sergio León <panicoenlaxbox@gmail.com>
2022-06-17 13:15:45 +0200

    Update conftest.py

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -9,19 +9,29 @@ from pyspark.sql import SparkSession
 
 
 @pytest.fixture(scope="session")
-def spark(request: FixtureRequest):
-    spark_: Optional[SparkSession] = None
+def spark(request: FixtureRequest) -> SparkSession:
+    # https://github.com/malexer/pytest-spark/issues/9#issue-434176947
+    # https://github.com/holdenk/spark-testing-base/issues/279
+    # https://stackoverflow.com/questions/52410267/how-many-spark-session-to-create
+    # https://www.bogotobogo.com/python/Multithread/python_multithreading_Using_Locks_with_statement_Context_Manager.php
 
-    request.addfinalizer(lambda: spark_.sparkContext.stop() if spark_ is not None else None)
+    spark_ = (
+        SparkSession.builder.master("local[*]")
+        .config("spark.sql.shuffle.partitions", 1)
+        .config("spark.default.parallelism", 1)
+        .config("spark.rdd.compress", False)
+        .config("spark.shuffle.compress", False)
+        .config("spark.ui.showConsoleProgress", False)
+        # .config("spark.ui.port", "8080")
+        # .config("spark.port.maxRetries", "30")
+        .getOrCreate()
+    )
+    # https://stackoverflow.com/questions/40608412/how-can-set-the-default-spark-logging-level
+    # spark_.sparkContext.setLogLevel("info")
 
-    def _():
-        nonlocal spark_
-        if spark_ is not None:
-            return spark_
-        spark_ = SparkSession.builder.getOrCreate()
-        return spark_
-
-    return _
+    # https://stackoverflow.com/questions/44058122/what-happens-if-sparksession-is-not-closed
+    request.addfinalizer(lambda: spark_.sparkContext.stop())
+    return spark_
 
 
 @pytest.fixture()
